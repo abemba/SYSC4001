0a727ea02ea87b761844eb9e139e69e591575a97
Samuel Dewan <samdewan@me.com>
2019-03-30 18:51:57 -0400

    Implement all commands on client and server side

--- Assignment-3/client.c
+++ Assignment-3/client.c
@@ -1,6 +1,273 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "msg_format.h"
+#include "msg_queue.h"
+
+struct command_t {
+    char *name;
+    void (*function)(void);
+};
+
+#define NUM_COMMANDS 8
+static struct command_t commands[];
+
+/** Message buffer */
+static struct message_t msg;
+
+
+/**
+ *  Get a single line from stdin.
+ *
+ *  @param buffer The buffer in which the line should be stored
+ *  @param buf_len The size of the buffer
+ *
+ *  @return 0 if successfull, a possitive integer otherwise
+ */
+static int get_line (char *buffer, int buf_len)
+{
+    char *retval;
+    retval = fgets(buffer, buf_len, stdin);
+    
+    if (!retval) {
+        // fgets failed
+        return 1;
+    }
+    
+    if ((retval = strchr(buffer, '\n')) == 0) {
+        // There is no new line in the input, the buffer was filled
+        return 1;
+    }
+    
+    // Discard newline
+    *retval = '\0';
+    
+    return 0;
+}
+
 
 int main (int argc, char **argv)
 {
+    // Get message queues
+    if (start_client()) {
+        fprintf(stderr, "Failed to connect to server.\n");
+        exit(1);
+    }
     
+    // Prepare message buffer
+    msg.client_pid = getpid();
+    
+    // Main loop
+    char buf[128];
+    
+    for (;;) {
+        printf("> ");
+        if (!get_line(buf, 128) && (strlen(buf) > 0)) {
+            int i;
+            for (i = 0; i < NUM_COMMANDS; i++) {
+                if (!strcasecmp(buf, commands[i].name)) {
+                    commands[i].function();
+                    break;
+                }
+            }
+            if (i == NUM_COMMANDS) {
+                printf("Unkown Command\n");
+            }
+        }
+    }
 }
+
+static void get_str (const char *prompt, char *result, int res_length)
+{
+    char buffer[res_length + 1];
+    printf("%s: ", prompt);
+    
+    for (;;) {
+        if (!get_line(buffer, res_length + 1)) {
+            if (strlen(buffer)) {
+                break;
+            } else {
+                printf("%s: ", prompt);
+            }
+        } else {
+            printf("%ss can be at most %d characters: ", prompt,
+                   (res_length - 1));
+            // Flush stdin
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+        }
+    }
+    
+    strncpy(result, buffer, res_length);
+}
+
+void insert_func (void)
+{
+    // Set up message
+    msg.type = REQUEST_INSERT;
+    
+    // Get employee information
+    get_str("Name", msg.insert.request.name, EMPLOYEE_NAME_LENGTH);
+    get_str("Department", msg.insert.request.department, EMPLOYEE_DEPT_LENGTH);
+    get_str("Employee number", msg.insert.request.number, EMPLOYEE_NUM_LENGTH);
+    
+    char buffer[11];
+    printf("Salary: ");
+    for (;;) {
+        if (!get_line(buffer, sizeof(buffer))  &&
+            (sscanf(buffer, " %d", &msg.insert.request.salary) == 1)) {
+            break;
+        } else {
+            printf("Please enter a valid integer: ");
+        }
+    }
+    
+    // Send to server and get response
+    send_message_to_server(msg);
+    get_response_from_server(&msg);
+    
+    if (msg.insert.response.type == REQUEST_FAILURE) {
+        printf("Failed to insert employee record.\n");
+    }
+}
+
+void check_name_func (void)
+{
+    // Set up message
+    msg.type = REQUEST_CHECK_NAME;
+    
+    // Get employee information
+    get_str("Employee number", msg.check_name.request, EMPLOYEE_NUM_LENGTH);
+    
+    // Send to server
+    send_message_to_server(msg);
+    get_response_from_server(&msg);
+    
+    if (msg.check_name.response.type == REQUEST_FAILURE) {
+        printf("No employee with number %s found.\n",
+               msg.check_name.request);
+    } else {
+        printf("%s\n", msg.check_name.response.name);
+    }
+}
+
+void check_dept_func (void)
+{
+    // Set up message
+    msg.type = REQUEST_CHECK_DEPT;
+    
+    // Get employee information
+    get_str("Employee number", msg.check_dept.request, EMPLOYEE_NUM_LENGTH);
+    
+    // Send to server
+    send_message_to_server(msg);
+    get_response_from_server(&msg);
+    
+    if (msg.check_dept.response.type == REQUEST_FAILURE) {
+        printf("No employee with number %s found.\n",
+               msg.check_dept.request);
+    } else {
+        printf("%s\n", msg.check_dept.response.department);
+    }
+}
+
+void check_salary_func (void)
+{
+    // Set up message
+    msg.type = REQUEST_CHECK_SALARY;
+    
+    // Get employee information
+    get_str("Employee number", msg.check_salary.request, EMPLOYEE_NUM_LENGTH);
+    
+    // Send to server
+    send_message_to_server(msg);
+    get_response_from_server(&msg);
+    
+    if (msg.check_salary.response.type == REQUEST_FAILURE) {
+        printf("No employee with number %s found.\n",
+               msg.check_salary.request);
+    } else {
+        printf("$%d\n", msg.check_salary.response.salary);
+    }
+}
+
+void check_num_func (void)
+{
+    // Set up message
+    msg.type = REQUEST_CHECK_EMPLOYEE_NUM;
+    
+    // Get employee information
+    get_str("Employee name", msg.check_employee_num.request,
+            EMPLOYEE_NAME_LENGTH);
+    
+    // Send to server
+    send_message_to_server(msg);
+    get_response_from_server(&msg);
+    
+    if (msg.check_employee_num.response.type == REQUEST_FAILURE) {
+        printf("No employee with name \"%s\" found.\n",
+               msg.check_employee_num.request);
+    } else {
+        printf("%s\n", msg.check_employee_num.response.number);
+    }
+}
+
+void list_dept_func (void)
+{
+    // Set up message
+    msg.type = REQUEST_LIST_DEPT;
+    
+    // Get employee information
+    get_str("Department", msg.list_dept.request, EMPLOYEE_DEPT_LENGTH);
+    
+    // Send to server
+    send_message_to_server(msg);
+    get_response_from_server(&msg);
+    
+    while (msg.list_dept.response.type == REQUEST_CONTINUE) {
+        printf("%s\n", msg.list_dept.response.number);
+        get_response_from_server(&msg);
+    }
+    
+    if (msg.list_dept.response.type == REQUEST_FAILURE) {
+        printf("Could not list employees of department \"%s\".\n",
+               msg.list_dept.request);
+    }
+}
+
+void delete_func (void)
+{
+    // Set up message
+    msg.type = REQUEST_DELETE;
+    
+    // Get employee information
+    get_str("Employee number", msg.delete.request, EMPLOYEE_NUM_LENGTH);
+    
+    // Send to server
+    send_message_to_server(msg);
+    get_response_from_server(&msg);
+    
+    if (msg.insert.response.type == REQUEST_FAILURE) {
+        printf("Failed to delete employee record.\n");
+    }
+}
+
+void quit_func (void)
+{
+    stop_client();
+    exit(0);
+}
+
+
+static struct command_t commands[NUM_COMMANDS] = {
+    {.name = "insert", .function = insert_func},
+    {.name = "check name", .function = check_name_func},
+    {.name = "check dept", .function = check_dept_func},
+    {.name = "check salary", .function = check_salary_func},
+    {.name = "check employee num", .function = check_num_func},
+    {.name = "list dept", .function = list_dept_func},
+    {.name = "delete", .function = delete_func},
+    {.name = "quit", .function = quit_func}
+};
--- Assignment-3/server.c
+++ Assignment-3/server.c
@@ -1,6 +1,242 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+#include <string.h>
+
+#include "array_list.h"
+
+#include "msg_format.h"
+#include "msg_queue.h"
+
+struct command_t {
+    enum request_type request_type;
+    void (*function)(struct message_t msg);
+};
+
+#define NUM_COMMANDS 7
+static struct command_t commands[];
+
+static volatile sig_atomic_t running;
+
+static struct array_list_t data;
+
+static void sigint_handler(int _)
+{
+    (void)_;
+    running = 0;
+    stop_server();
+}
 
 int main (int argc, char **argv)
 {
+    running = 1;
+    // Register handler for SIGINT
+    signal(SIGINT, sigint_handler);
     
+    // Create list for data
+    if (array_list_create(&data, 2)) {
+        fprintf(stderr, "Failed to create array list.\n");
+        exit(1);
+    }
+    
+    // Create message queues
+    if (start_server()) {
+        fprintf(stderr, "Failed to create message queues.\n");
+        exit(1);
+    }
+    
+    // Main loop
+    struct message_t msg;
+    
+    while (running) {
+        if (get_client_request(&msg) != 0) {
+            break;
+        }
+        
+        int i;
+        for (i = 0; i < NUM_COMMANDS; i++) {
+            if (commands[i].request_type == msg.type) {
+                commands[i].function(msg);
+                break;
+            }
+        }
+        
+        if (i == NUM_COMMANDS) {
+            printf("Received unkown command %jd\n", (intmax_t) msg.type);
+        }
+    }
+    
+    // Remove message queues
+    stop_server();
+    
+    // Free data
+    array_list_free(&data);
 }
+
+void insert_func (struct message_t msg)
+{
+    void *new = malloc(sizeof(struct employee_t));
+    memcpy(new, &msg.insert.request, sizeof(struct employee_t));
+    
+    if (array_list_append(&data, new)) {
+        msg.insert.response.type = REQUEST_FAILURE;
+    } else {
+        msg.insert.response.type = REQUEST_END;
+    }
+    
+    if (send_response_to_client(msg)) {
+        fprintf(stderr, "Failed to send reponse to client with pid %jd\n",
+                (intmax_t) msg.client_pid);
+    }
+}
+
+void check_name_func (struct message_t msg)
+{
+    int i;
+    for (i = 0; i < data.length; i++) {
+        struct employee_t *employee = array_list_get(&data, i);
+        if (!strcmp(employee->number, msg.check_name.request)) {
+            strncpy(msg.check_name.response.name, employee->name,
+                    EMPLOYEE_NAME_LENGTH);
+            break;
+        }
+    }
+    
+    if (i >= data.length) {
+        msg.check_name.response.type = REQUEST_FAILURE;
+    } else {
+        msg.check_name.response.type = REQUEST_END;
+    }
+    
+    if (send_response_to_client(msg)) {
+        fprintf(stderr, "Failed to send reponse to client with pid %jd\n",
+                (intmax_t) msg.client_pid);
+    }
+}
+
+void check_dept_func (struct message_t msg)
+{
+    int i;
+    for (i = 0; i < data.length; i++) {
+        struct employee_t *employee = array_list_get(&data, i);
+        if (!strcmp(employee->number, msg.check_dept.request)) {
+            strncpy(msg.check_dept.response.department, employee->department,
+                    EMPLOYEE_DEPT_LENGTH);
+            break;
+        }
+    }
+    
+    if (i >= data.length) {
+        msg.check_dept.response.type = REQUEST_FAILURE;
+    } else {
+        msg.check_dept.response.type = REQUEST_END;
+    }
+    
+    if (send_response_to_client(msg)) {
+        fprintf(stderr, "Failed to send reponse to client with pid %jd\n",
+                (intmax_t) msg.client_pid);
+    }
+}
+
+void check_salary_func (struct message_t msg)
+{
+    int i;
+    for (i = 0; i < data.length; i++) {
+        struct employee_t *employee = array_list_get(&data, i);
+        if (!strcmp(employee->number, msg.check_salary.request)) {
+            msg.check_salary.response.salary = employee->salary;
+            break;
+        }
+    }
+    
+    if (i >= data.length) {
+        msg.check_salary.response.type = REQUEST_FAILURE;
+    } else {
+        msg.check_salary.response.type = REQUEST_END;
+    }
+    
+    if (send_response_to_client(msg)) {
+        fprintf(stderr, "Failed to send reponse to client with pid %jd\n",
+                (intmax_t) msg.client_pid);
+    }
+}
+
+void check_num_func (struct message_t msg)
+{
+    int i;
+    for (i = 0; i < data.length; i++) {
+        struct employee_t *employee = array_list_get(&data, i);
+        if (!strcmp(employee->name, msg.check_employee_num.request)) {
+            strncpy(msg.check_employee_num.response.number, employee->number,
+                    EMPLOYEE_NUM_LENGTH);
+            break;
+        }
+    }
+    
+    if (i >= data.length) {
+        msg.check_employee_num.response.type = REQUEST_FAILURE;
+    } else {
+        msg.check_employee_num.response.type = REQUEST_END;
+    }
+    
+    if (send_response_to_client(msg)) {
+        fprintf(stderr, "Failed to send reponse to client with pid %jd\n",
+                (intmax_t) msg.client_pid);
+    }
+}
+
+void list_dept_func (struct message_t msg)
+{
+    for (int i = 0; i < data.length; i++) {
+        struct employee_t *employee = array_list_get(&data, i);
+        if (!strcmp(employee->department, msg.list_dept.request)) {
+            msg.list_dept.response.type = REQUEST_CONTINUE;
+            strncpy(msg.list_dept.response.number, employee->number,
+                    EMPLOYEE_NUM_LENGTH);
+            if (send_response_to_client(msg)) {
+                fprintf(stderr, "Failed to send reponse to client with pid %jd\n",
+                        (intmax_t) msg.client_pid);
+            }
+        }
+    }
+    
+    msg.list_dept.response.type = REQUEST_END;
+    if (send_response_to_client(msg)) {
+        fprintf(stderr, "Failed to send reponse to client with pid %jd\n",
+                (intmax_t) msg.client_pid);
+    }
+}
+
+void delete_func (struct message_t msg)
+{
+    int i;
+    for (i = 0; i < data.length; i++) {
+        struct employee_t *employee = array_list_get(&data, i);
+        if (!strcmp(employee->number, msg.delete.request)) {
+            array_list_remove(&data, i);
+            break;
+        }
+    }
+    
+    if (i >= data.length) {
+        msg.check_employee_num.response.type = REQUEST_FAILURE;
+    } else {
+        msg.check_employee_num.response.type = REQUEST_END;
+    }
+    
+    if (send_response_to_client(msg)) {
+        fprintf(stderr, "Failed to send reponse to client with pid %jd\n",
+                (intmax_t) msg.client_pid);
+    }
+}
+
+
+static struct command_t commands[NUM_COMMANDS] = {
+    {.request_type = REQUEST_INSERT, .function = insert_func},
+    {.request_type = REQUEST_CHECK_NAME, .function = check_name_func},
+    {.request_type = REQUEST_CHECK_DEPT, .function = check_dept_func},
+    {.request_type = REQUEST_CHECK_SALARY, .function = check_salary_func},
+    {.request_type = REQUEST_CHECK_EMPLOYEE_NUM, .function = check_num_func},
+    {.request_type = REQUEST_LIST_DEPT, .function = list_dept_func},
+    {.request_type = REQUEST_DELETE, .function = delete_func}
+};
